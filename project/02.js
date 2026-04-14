4ba96e9b83acac14c936d3045b1308c6ae80c19c
Sudip1232 <sudipghosh9883@gmail.com>
2026-04-14 23:42:47 +0530

    done

--- project/02.js
+++ project/02.js
@@ -4,7 +4,9 @@ const form = document.querySelector('form');
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
-
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
 
   if (height === '' || height < 0 || isNaN(height)) {
     results.innerHTML = `Please give a valid height ${height}`;
